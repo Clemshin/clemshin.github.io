23a8ab8c4767c0cdaff545a3f9a45d379fb10822
Clemshin <mistysimsss@gmail.com>
2024-07-16 13:59:08 -0400

    Update switzerland.js

--- switzerland.js
+++ switzerland.js
@@ -1,1 +1,10 @@
+//46.8182 N, 8.2275 E -> 46.8182, 8.2275
+let map = L.map("map", {
+  center: {46.8182, 8.2275},
+  zoom: 15
+});
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);

368a267fa8d0c7527830fee3eccbadce9d59e4b3
Clemshin <mistysimsss@gmail.com>
2024-07-18 13:09:01 -0400

    Update switzerland.js

--- switzerland.js
+++ switzerland.js
@@ -2,7 +2,7 @@
 let map = L.map("map", {
   center: [46.8182, 8.2275],
   zoom: 15
-);
+});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -15,4 +15,4 @@ L.circle([46.8182, 8.2275], {
         color: 'black',
         radious: 1000
   }).bindPopup(`<iframe width="280" height="120" src="https://www.youtube.com/embed/FJJtIxuDr9s?si=i8Ofrh5gC7sWrCA0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`)
-}).addTo(map);
+).addTo(map);
